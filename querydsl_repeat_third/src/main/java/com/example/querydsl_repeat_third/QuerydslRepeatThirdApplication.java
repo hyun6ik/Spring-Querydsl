45982812d6606db5eeed53965bbf9dd171de672b
hyun6ik <fkanwm@naver.com>
2021-04-20 23:17:47 +0900

    프로젝트 생성

---
+++ querydsl_repeat_third/src/main/java/com/example/querydsl_repeat_third/QuerydslRepeatThirdApplication.java
@@ -0,0 +1,13 @@
+package com.example.querydsl_repeat_third;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerydslRepeatThirdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerydslRepeatThirdApplication.class, args);
+	}
+
+}
---
+++ querydsl_repeat_third/src/test/java/com/example/querydsl_repeat_third/QuerydslRepeatThirdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.querydsl_repeat_third;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerydslRepeatThirdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
