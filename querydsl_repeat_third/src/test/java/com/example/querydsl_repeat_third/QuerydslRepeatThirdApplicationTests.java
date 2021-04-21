3a825db91287fed9bac144238a8854278c10dc4b
hyun6ik <fkanwm@naver.com>
2021-04-21 22:06:03 +0900

    Querydsl 설정과 검증

--- querydsl_repeat_third/src/test/java/com/example/querydsl_repeat_third/QuerydslRepeatThirdApplicationTests.java
+++ querydsl_repeat_third/src/test/java/com/example/querydsl_repeat_third/QuerydslRepeatThirdApplicationTests.java
@@ -1,13 +1,42 @@
 package com.example.querydsl_repeat_third;
 
+import com.example.querydsl_repeat_third.entity.Hello;
+import com.example.querydsl_repeat_third.entity.QHello;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
+@Transactional
 class QuerydslRepeatThirdApplicationTests {
+
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		QHello qHello = new QHello("h");
+
+		Hello result = query
+				.selectFrom(qHello)
+				.fetchOne();
+
+		assertThat(result).isEqualTo(hello);
+		assertThat(result.getId()).isEqualTo(hello.getId());
+
+
 	}
 
 }
